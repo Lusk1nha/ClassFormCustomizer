24e0378f816ea013722f6763c08e79f09c018048
unknown <lucaspedro517@gmail.com>
2022-06-21 15:14:06 -0300

    SPFX 1.15 structure

---
+++ src/extensions/classFormCustomizer/loc/myStrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IClassFormCustomizerFormCustomizerStrings {
+  Save: string;
+  Cancel: string;
+  Close: string;
+}
+
+declare module 'ClassFormCustomizerFormCustomizerStrings' {
+  const strings: IClassFormCustomizerFormCustomizerStrings;
+  export = strings;
+}
